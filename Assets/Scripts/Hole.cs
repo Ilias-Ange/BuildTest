caaba13e92589821382b1ba0420e71265d8ce40e
Ilias-Ange <83338859+Ilias-Ange@users.noreply.github.com>
2021-09-27 02:34:15 +0900

    Add files via upload

---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    //どのボールを吸い寄せるかタグで指定
+    public string targetTag;
+    bool isHolding;
+
+    //ボールが入っているかを返す
+    public bool IsHolding(){
+        return isHolding;
+    }
+
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject.tag == targetTag){
+            isHolding = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other){
+        if (other.gameObject.tag == targetTag){
+            isHolding = false;
+        }
+    }
+
+    void OnTriggerStay(Collider other){
+        //コライダに触れているオブジェクトのRigidbodyコンポーネントを取得
+        Rigidbody r = other.gameObject.GetComponent<Rigidbody>();
+
+        //ボールがどの方向にあるか計算
+        Vector3 direction = other.gameObject.transform.position - transform.position;
+        direction.Normalize();
+
+        //タグに応じてボールに力を加える
+        if (other.gameObject.tag == targetTag){
+            //中心地点でボールを止めるため速度を減速させる
+            r.velocity *= 0.9f;
+            r.AddForce(direction * -20.0f, ForceMode.Acceleration);
+        } else {
+            r.AddForce(direction * 80.0f, ForceMode.Acceleration);
+        }
+    }
+}
